d8120aff5db06d69983436c8fb3f0b36b740a42a
JPalmer82 <jaretpalmer00@gmail.com>
2025-05-08 01:12:10 -0500

    08MAY_FinalPush
    Finally figured out how to create a menu and how to create folders using that menu and it worked when i ran it on a test project. Updated ReadMe to explain how the code works.

---
+++ TechDir_PersonalProj/ReadMe.md
@@ -0,0 +1,14 @@
+# Starter Project Folder Creation Tool
+
+This is a tool to create the starter files we always create in Unreal Engine, rather than manually creating the folders and naming them this tool does both.
+
+<img src = "assets/StartingProjectFolders.PNG">
+
+# How to Run
+
+* Open the Output Log Menu, change the bottom left "Cmd" to "Python".
+* Copy the code up until the commented out part of the UnrealStarterFolder.py and paste into the textbox next to "Python".
+* A new menu will appear in the top part of the Unreal Engine Window named "Starting Project Folders".
+* Click on the menu and click on the drop down button named "Create Project Folders".
+* _MyFiles folder along with Assets, Blueprints, Levels, and Materials subfolders will be created under "Content" folder.
+* The created menu will be gone the next time you open the Unreal Engine Project.
--- TechDir_PersonalProj/src/UnrealStarterFolders.py
+++ TechDir_PersonalProj/src/UnrealStarterFolders.py
@@ -1,21 +1,67 @@
 import unreal
 
-def ListMenu(num = 1000):
-    menuList = set()
-    for i in range(num):
-        obj = unreal.find_object(None, "/Engine/Transient.ToolMenus_0:RegisteredMenu_%s" % i)
-        if not obj:
-            obj = unreal.find_object(None, f"/Engine/Transient.ToolMenus_0:ToolMenu_{i}") # for backwards compatibility
+def main():
+    menu = unreal.ToolMenus.get()
+    mainMenu = menu.find_menu("LevelEditor.MainMenu")
+    customMenu = mainMenu.add_sub_menu("Custom Menu", "Python Automation", "StartingProjName", "Starting Project Folders")
+    menu.refresh_all_widgets()
 
-            if not obj:
-                continue
+    newFolders = CreateProjectFolders()
+    newFolders.init_entry(
+        owner_name = customMenu.menu_name,
+        menu = customMenu.menu_name,
+        section = "Create Folders",
+        name = "Create Project Folders",
+        label = "Create Project Folders",
+        tool_tip = "Custom Script Entry"
+    )
+    newFolders.register_menu_entry()
+    
 
-        menuName = str(obj.menu_name)
-        if menuName == "None":
-            continue
 
-        menuList.add(menuName)
 
-    return list(menuList)
+@unreal.uclass()
+class CreateProjectFolders(unreal.ToolMenuEntryScript):
+    @unreal.ufunction(override = True)
+    def execute(self, context):
+        assetLibrary = unreal.EditorAssetLibrary()
+        myFiles = "/Game/_MyFiles"
 
-print(ListMenu())
+        assetLibrary.make_directory(myFiles)
+
+        assetFolder = "/Game/_MyFiles/Assets"
+        bpFolder = "/Game/_MyFiles/Blueprints"
+        levelFolder = "/Game/_MyFiles/Levels"
+        matFolder = "/Game/_MyFiles/Materials"
+
+        assetLibrary.make_directory(assetFolder)
+        assetLibrary.make_directory(bpFolder)
+        assetLibrary.make_directory(levelFolder)
+        assetLibrary.make_directory(matFolder)
+
+
+main()
+
+
+# -------------------------- This function was used to find label names and print them out, if you want to put the starting folders
+# -------------------------- tool somewhere else, you can run this to find a new name for the mainMenu = menu.find_menu("Put new name here")
+
+# def ListMenu(num = 1000):
+#     menuList = set()
+#     for i in range(num):
+#         obj = unreal.find_object(None, "/Engine/Transient.ToolMenus_0:RegisteredMenu_%s" % i)
+#         if not obj:
+#             obj = unreal.find_object(None, f"/Engine/Transient.ToolMenus_0:ToolMenu_{i}") # for backwards compatibility
+
+#             if not obj:
+#                 continue
+
+#         menuName = str(obj.menu_name)
+#         if menuName == "None":
+#             continue
+
+#         menuList.add(menuName)
+
+#     return list(menuList)
+
+# print(ListMenu())
